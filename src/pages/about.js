bd7c5b227811e1d799460e466d31c4061bfdf4ac
Alina8177 <83888270+Alina8177@users.noreply.github.com>
2021-12-23 10:10:49 -0500

    updated App.css & SignIn.js & about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -16,7 +16,7 @@ const About = () => {
         alt="gym-1" />  </Link>
       <div style={{ display: "flex", flexDirection: 'column', gap: 10, alignItems: "center", marginTop: 15, height: '500px', padding: '35px', justifyContent: 'center' }}>
       <h1 style={{ fontSize: '35px', margin: '30px 0'}}>Welcome to the Gym World.</h1>
-      <p style={{ fontSize: '25px'}}>Hi, we’re Blink Fitness, and we believe that exercise isn’t just about how it makes you look, but also how it makes you feel. Blink is a motivating space with a staff of mood-lifters. We take pride in challenging fitness industry norms and celebrating every unique body.</p>
+      <p style={{ fontSize: '25px'}}>Hi, we’re The Best Fitness, and we believe that exercise isn’t just about how it makes you look, but also how it makes you feel. Blink is a motivating space with a staff of mood-lifters. We take pride in challenging fitness industry norms and celebrating every unique body.</p>
       </div>
     </div>
   );
