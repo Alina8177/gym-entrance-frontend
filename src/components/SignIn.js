130fa775279bee95e4dbabd81c01a0474eede2ab
Alina8177 <83888270+Alina8177@users.noreply.github.com>
2021-12-23 10:38:50 -0500

    updated App.js & SignIn.js

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -14,7 +14,6 @@ const SignIn = (props) => {
         <Link  to="/">  <img
           id="gym-logo"
           src="https://images.unsplash.com/photo-1526506118085-60ce8714f8c5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
-          style={{ width: '60vw'}}
           alt="gym-1"
         />  </Link>
 
