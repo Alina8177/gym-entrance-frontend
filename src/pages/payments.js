acb29fd89cf639668c746a87d0b0481147cd5fbf
Alina8177 <83888270+Alina8177@users.noreply.github.com>
2021-12-23 09:52:37 -0500

    updated payments.js

--- src/pages/payments.js
+++ src/pages/payments.js
@@ -15,7 +15,7 @@ const Payments = () => {
         id="Programs-logo"
         src="https://blogs.worldbank.org/sites/default/files/2021-06/government_payments_shutterstock_252335677_780.jpg"
         alt="gym-1" />  </Link>
-      <div>
+      <div id="payments">
       <h1 style={{ fontSize: '35px', margin: '30px 0'}}>Payments</h1>
       <h5 style={{ fontSize: '20px', margin: '20px 0'}}>Choose your payment method</h5>
       <ul style={{ listStyle: 'none', margin: '35px 0', fontSize: '25px'}}>
