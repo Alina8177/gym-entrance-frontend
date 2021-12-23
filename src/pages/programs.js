91d6174c8fb5cd3148cf6ee55190436609367a01
Alina8177 <83888270+Alina8177@users.noreply.github.com>
2021-12-23 09:43:01 -0500

    updated App.css & programs.js

--- src/pages/programs.js
+++ src/pages/programs.js
@@ -14,7 +14,6 @@ const Programs = () => {
       <Link to="/">  <img
         id="Programms-logo"
         src="https://images.unsplash.com/photo-1602668253129-0730d95f1b27?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTczNnx8Z3ltfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-        style={{width: '65vw', height: '70%'}}
         alt="gym-1" />  </Link>
       <div>
       <h1 style={{ fontSize: '35px', margin: '30px 0'}}>Choose best programs for you</h1>
